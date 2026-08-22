f7953788c62d0b3c5df890603d1f6377264d85c6
Huỳnh Nguyễn <107023404+nguyenxuanhuynh2003@users.noreply.github.com>
2026-08-22 11:54:05 +0700

    Setup Html

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+//
+// This is The Scripts used for Simply Theme
+//
+function main() {
+	(function () {
+		'use strict'
+		//Script
+		//-----------------------------------
+		jQuery(document).ready(function ($) {
+			var wd = $(window).width();
+
+			var wd = jQuery(window).width();
+			
+
+			
+			
+		});
+
+
+
+
+	}());
+}
+main();
